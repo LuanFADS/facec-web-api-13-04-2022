f443274b755556c6aa79d52912616c9d21283967
Luan Francisco <luancianorte@gmail.com>
2022-04-13 22:34:15 -0300

    Second commit

--- FACEC.WebAPI/Startup.cs
+++ FACEC.WebAPI/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -44,7 +45,10 @@ namespace FACEC.WebAPI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/world", async (context) =>
+                {
+                    await context.Response.WriteAsync("Hello World");
+                });
             });
         }
     }
